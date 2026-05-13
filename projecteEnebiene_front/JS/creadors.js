bf4943bf291ad78c0f9fa586f2cace50005344a9
Ivan Mesquida <mara72737@gmail.com>
2026-05-13 08:57:43 +0200

    Llamada a Creadores Funcional

--- projecteEnebiene_front/JS/creadors.js
+++ projecteEnebiene_front/JS/creadors.js
@@ -1,7 +1,29 @@
 async function mostrarCreadores() {
-    // Ejemplo para obtener los videojuegos
-    const creadores = await consultar("SELECT c.id, c.nombre, c.pais, c.imagen, COUNT(v.id) AS total_juegos FROM creadores c LEFT JOIN videojuegos v ON v.id_creador = c.id GROUP BY c.id ORDER BY c.nombre ASC");
-    console.log(creadores); // Aquí verás la lista en la consola del navegador
+    const creadors = await consultar("SELECT c.id, c.nombre, c.pais, c.imagen, COUNT(v.id) AS total_juegos FROM creadores c LEFT JOIN videojuegos v ON v.id_creador = c.id GROUP BY c.id ORDER BY c.nombre ASC");
+    //console.log(creadores); 
+    const container = document.querySelector("#lista-creadors");
+
+    if(!creadors) {
+        alert("Error en Carregar els Creadors");
+        return;
+    }
+
+    creadors.forEach(creador => {
+        const contenedorCreador = document.createElement("div");
+        contenedorCreador.classList.add("contenedorCreador");
+
+        contenedorCreador.innerHTML = `
+            <img src="img/${creador.imagen}" alt="Logo de ${creador.nombre}" class="img-creador">
+            <div class="info">
+                <h2>${creador.nombre}</h2>
+                <p><strong>País:</strong> ${creador.pais}</p>
+                <p><strong>Juegos creados:</strong> ${creador.total_juegos}</p>
+            </div>
+        `;
+
+        container.appendChild(contenedorCreador);
+    });
 }
 
 mostrarCreadores();
+
