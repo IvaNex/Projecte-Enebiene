3c9b6aade1b1e634867fe004c09fcfde4fe80215
Jeroni Sureda <jeroni3832@gmail.com>
2026-05-15 22:22:08 +0200

    primera version de filtro no va

--- projecteEnebiene_front/JS/creadors.js
+++ projecteEnebiene_front/JS/creadors.js
@@ -1,5 +1,23 @@
-async function mostrarCreadores() {
-    const creadors = await consultar("SELECT c.id, c.nombre, c.pais, c.imagen, COUNT(v.id) AS total_juegos FROM creadores c LEFT JOIN videojuegos v ON v.id_creador = c.id GROUP BY c.id ORDER BY c.nombre ASC");
+async function mostrarCreadores(orden = "nombre-asc") {
+
+    let sql = "SELECT c.id, c.nombre, c.pais, c.imagen, COUNT(v.id) AS total_juegos FROM creadores c LEFT JOIN videojuegos v ON v.id_creador = c.id GROUP BY c.id"
+
+    switch (orden) {
+
+        case "juegos-asc":
+            sql += " ORDER BY TOTAL_juegos ASC";
+            break;
+        case "juegos-desc":
+            sql += " ORDER BY total_juegos DESC";
+            break;
+        case "nombre-desc":
+            sql += " ORDER BY c.nombre DESC";
+            break;
+        default:
+            sql += " ORDER BY c.nombre ASC";
+
+    }
+    const creadors = await consultar(sql);
     //console.log(creadores); 
     const container = document.querySelector("#lista-creadors");
 
@@ -7,13 +25,16 @@ async function mostrarCreadores() {
         alert("Error en Carregar els Creadors");
         return;
     }
+    container.innerHTML = "";
 
     creadors.forEach(creador => {
         const contenedorCreador = document.createElement("div");
         contenedorCreador.classList.add("contenedorCreador");
 
         contenedorCreador.innerHTML = `
-            <img src="img/${creador.imagen}" alt="Logo de ${creador.nombre}" class="img-creador">
+            <a herf="productos.html?creadors=${creador.id}">
+            <img src="imagenes/${creador.imagen}" alt="Logo de ${creador.nombre}" class="img-creador">
+            </a>
             <div class="info">
                 <h2>${creador.nombre}</h2>
                 <p><strong>País:</strong> ${creador.pais}</p>
@@ -24,6 +45,17 @@ async function mostrarCreadores() {
         container.appendChild(contenedorCreador);
     });
 }
-
 mostrarCreadores();
+
+
+const selector = document.querySelector("#ordenar-creadors");
+
+if (selector) {
+    selector.addEventListener("changer", (e) => {
+        const valorElegido = e.target.value;
+        console.log("Cambiando orden a: " + valorElegido);
+        mostrarCreadores(e.target.value);
+    });
+}
+
 
